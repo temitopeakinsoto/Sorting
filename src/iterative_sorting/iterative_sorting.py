ee7cc5c859e0d0a7586eaad2eacce28f3641f58b
Temitope Akinsoto <takinsoto@gmail.com>
2020-02-18 00:12:41 +0100

    selection sort implemented

--- src/iterative_sorting/iterative_sorting.py
+++ src/iterative_sorting/iterative_sorting.py
@@ -6,6 +6,11 @@ def selection_sort( arr ):
         smallest_index = cur_index
         # TO-DO: find next smallest element
         # (hint, can do in 3 loc) 
+        for j in range(cur_index + 1, len(arr)):
+            if arr[smallest_index] > arr[j]:
+                smallest_index = j
+        (arr[cur_index], arr[smallest_index]) = (arr[smallest_index], arr[cur_index])
+
              
 
 
